917c261b43edff168743fea8c64c7bad07288562
Thong Nguyen <nguyentrthong95@gmail.com>
2019-04-07 11:01:49 +0700

    Tweak to make oh my zsh can be reinstall if install failed

--- install.sh
+++ install.sh
@@ -29,6 +29,9 @@ done
 if [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
 	echo 'Oh-my-zsh is installed'
 else
+	if [ -d ~/.oh-my-zsh ]; then
+		rm -rf ~/.oh-my-zsh
+	fi
 	echo 'Oh-my-zsh is not installed'
 	echo 'Installing oh-my-zsh'
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null
