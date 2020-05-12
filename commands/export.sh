b9027a0b1a0f5b1e66483da809c4167061134eed
Thong Nguyen <thong.nt@teko.vn>
2020-05-12 11:59:15 +0700

    Export commands

--- commands/export.sh
+++ commands/export.sh
@@ -3,9 +3,27 @@
 CLI_DIR=$1
 NVIM_DIR=~/.config/nvim
 
+echo "Backup config files"
+mkdir -p $CLI_DIR/backup/nvim
+cp    $NVIM_DIR/init.vim          $CLI_DIR/backup/nvim
+cp    $NVIM_DIR/coc-settings.json $CLI_DIR/backup/nvim
+cp -r $NVIM_DIR/colors            $CLI_DIR/backup/nvim
+cp -r $NVIM_DIR/autoload          $CLI_DIR/backup/nvim
+
+cp    $HOME/.vimrc $CLI_DIR/backup
+cp    $HOME/.tmux.conf $CLI_DIR/backup
+cp    $HOME/.bashrc $CLI_DIR/backup
+cp    $HOME/.zshrc $CLI_DIR/backup
+
+
 cp    $CLI_DIR/config_files/nvim/init.vim           $NVIM_DIR
 cp    $CLI_DIR/config_files/nvim/coc-settings.json  $NVIM_DIR
 cp -r $CLI_DIR/config_files/nvim/colors             $NVIM_DIR
 cp -r $CLI_DIR/config_files/nvim/autoload           $NVIM_DIR
-echo "NVIM config files exported"
 
+cp    $CLI_DIR/config_files/.bashrc     $HOME
+cp    $CLI_DIR/config_files/.zshrc      $HOME
+cp    $CLI_DIR/config_files/.tmux.conf  $HOME
+cp    $CLI_DIR/config_files/.vimrc      $HOME
+
+echo "Exported config files"
