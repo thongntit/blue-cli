e863101e37115df5be5d84a170561c6cfd4cecc9
Thong Nguyen <nguyentrthong95@gmail.com>
2020-08-08 17:14:44 +0000

    Change oh-my-tmux config file

--- commands/export.sh
+++ commands/export.sh
@@ -11,7 +11,7 @@ cp -r $NVIM_DIR/colors            $CLI_DIR/backup/nvim
 cp -r $NVIM_DIR/autoload          $CLI_DIR/backup/nvim
 
 cp    $HOME/.vimrc $CLI_DIR/backup
-cp    $HOME/.tmux.conf $CLI_DIR/backup
+cp    $HOME/.tmux.conf.local $CLI_DIR/backup
 cp    $HOME/.bashrc $CLI_DIR/backup
 cp    $HOME/.zshrc $CLI_DIR/backup
 
@@ -21,9 +21,9 @@ cp    $CLI_DIR/config_files/nvim/coc-settings.json  $NVIM_DIR
 cp -r $CLI_DIR/config_files/nvim/colors             $NVIM_DIR
 cp -r $CLI_DIR/config_files/nvim/autoload           $NVIM_DIR
 
-cp    $CLI_DIR/config_files/.bashrc     $HOME
-cp    $CLI_DIR/config_files/.zshrc      $HOME
-cp    $CLI_DIR/config_files/.tmux.conf  $HOME
-cp    $CLI_DIR/config_files/.vimrc      $HOME
+cp    $CLI_DIR/config_files/.bashrc           $HOME
+cp    $CLI_DIR/config_files/.zshrc            $HOME
+cp    $CLI_DIR/config_files/.tmux.conf.local  $HOME
+cp    $CLI_DIR/config_files/.vimrc            $HOME
 
 echo "Exported config files"
--- commands/revert.sh
+++ commands/revert.sh
@@ -8,9 +8,9 @@ cp    $CLI_DIR/backup/nvim/coc-settings.json  $NVIM_DIR
 cp -r $CLI_DIR/backup/nvim/colors             $NVIM_DIR
 cp -r $CLI_DIR/backup/nvim/autoload           $NVIM_DIR
 
-cp    $CLI_DIR/backup/.bashrc     $HOME
-cp    $CLI_DIR/backup/.zshrc      $HOME
-cp    $CLI_DIR/backup/.tmux.conf  $HOME
-cp    $CLI_DIR/backup/.vimrc      $HOME
+cp    $CLI_DIR/backup/.bashrc           $HOME
+cp    $CLI_DIR/backup/.zshrc            $HOME
+cp    $CLI_DIR/backup/.tmux.conf.local  $HOME
+cp    $CLI_DIR/backup/.vimrc            $HOME
 
 echo "Reverted config files"
