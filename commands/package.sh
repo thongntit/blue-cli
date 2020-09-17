cd249901132c5bfcd59bbd7db6a2ba24e1f7c614
Thong Nguyen <nanolove95@gmail.com>
2020-09-17 22:39:06 +0700

    :zap: Install Neovim

--- commands/package.sh
+++ commands/package.sh
@@ -10,10 +10,18 @@ PM="apt-get"
 # Update package list
 $PM update
 # Check if program is installed, if not install it.
-declare -a arr=( "git" "zsh" "curl" "tmux" "vim" "fonts-powerline")
+declare -a arr=( "git" "zsh" "curl" "tmux" "vim" "fonts-powerline" "zsh-antigen")
 for i in "${arr[@]}"
 do
     echo "Installing $i"
     $PM install $i -y
 done
 
+if [ -f /usr/bin/nvim.appimage ]; then
+	echo 'Neovim is installed'
+else
+	echo 'Neovim was not installed'
+	echo 'Installing Neovim'
+	curl -Lo /usr/bin/nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
+  chmod +x /usr/bin/nvim.appimage
+fi
