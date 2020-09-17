04d0a9ae2b932b811479d5a5fe02604afebc0f8f
Thong Nguyen <nanolove95@gmail.com>
2020-09-17 21:51:07 +0700

    :bug: fix bug that can't export with nvim folder

--- commands/export.sh
+++ commands/export.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 CLI_DIR=$1
-NVIM_DIR=$HOME/.config/nvim
+NVIM_DIR=$HOME/.config/nvim/
 
 echo "Backup config files"
-mkdir -p $CLI_DIR/backup/nvim
-cp    $NVIM_DIR/init.vim          $CLI_DIR/backup/nvim
-cp    $NVIM_DIR/coc-settings.json $CLI_DIR/backup/nvim
-cp -r $NVIM_DIR/colors            $CLI_DIR/backup/nvim
-cp -r $NVIM_DIR/autoload          $CLI_DIR/backup/nvim
+if [ -d $HOME/.config/nvim/ ]
+then
+    mkdir -p $CLI_DIR/backup/nvim/
+    cp    $NVIM_DIR/init.vim          $CLI_DIR/backup/nvim
+    cp    $NVIM_DIR/coc-settings.json $CLI_DIR/backup/nvim
+    cp -r $NVIM_DIR/colors            $CLI_DIR/backup/nvim
+    cp -r $NVIM_DIR/autoload          $CLI_DIR/backup/nvim
+fi
+
 
 cp    $HOME/.vimrc $CLI_DIR/backup
 cp    $HOME/.tmux.conf.local $CLI_DIR/backup
 cp    $HOME/.bashrc $CLI_DIR/backup
 cp    $HOME/.zshrc $CLI_DIR/backup
 
+if [ ! -d $HOME/.config/nvim ]
+then
+    mkdir -p $HOME/.config/nvim/
+fi
 
 cp    $CLI_DIR/config_files/nvim/init.vim           $NVIM_DIR
 cp    $CLI_DIR/config_files/nvim/coc-settings.json  $NVIM_DIR
