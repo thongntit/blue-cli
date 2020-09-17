ddc732333f0baa6c3c0b90678d38e640c5e5837a
Thong Nguyen <nanolove95@gmail.com>
2020-09-17 22:45:26 +0700

    :zap: Install zgen

--- commands/plugin.sh
+++ commands/plugin.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-### ZSH
-# Check if oh my zsh is installed
-if [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
-	echo 'Oh-my-zsh is installed'
-else
-	if [ -d ~/.oh-my-zsh ]; then
-		rm -rf ~/.oh-my-zsh
-	fi
-	echo 'Oh-my-zsh is not installed'
-	echo 'Installing oh-my-zsh'
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null
-fi
-
 # Check if zsh-autosuggestion is installed
-if [ -d ~/.oh-my-zsh/plugins/zsh-autosuggestions ]
+if [ -d ~/.zgen ]
 then
-	echo "Zsh-autosuggestions is installed"
+	echo "zgen was installed"
 else
-	echo "Zsh-autosuggestions is not installed"
-	echo "Installing zsh-autosuggestions"
-	mkdir -pv ~/.oh-my-zsh/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions > /dev/null
+	echo "zgen has not been installed"
+	echo "Installing zgen"
+	git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 fi
 
 ### TMUX
